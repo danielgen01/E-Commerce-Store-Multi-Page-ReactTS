673cd499f6c9090319cd66236c3e138188d9840a
Daniel <gendik38@gmail.com>
2023-02-14 13:44:49 +0100

    make responsive layout for shop items page

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -7,7 +7,7 @@ const Main:React.FC = () => {
     <div className='Main'>
       <div className='shop-items-ctn'>
         {/* PRODUCT 1 */}
-        <div className='item-ctn'>
+        <div className='item-ctn col-s-6 col-4'>
           <h2>iPhone 12</h2>
           <img src='https://files.refurbed.com/ii/iphone-12-1607327724.jpg?t=resize&h=600&w=800'
           className='product-img' />
@@ -21,7 +21,7 @@ const Main:React.FC = () => {
         </div>
 
          {/* PRODUCT 2 */}
-         <div className='item-ctn'>
+         <div className='item-ctn col-s-6 col-4'>
           <h2>iPhone SE (2020)</h2>
           <img src='https://files.refurbed.com/ii/iphone-se-2020-1587013419.jpg?t=resize&h=600&w=800'
           className='product-img' />
@@ -35,7 +35,7 @@ const Main:React.FC = () => {
         </div>
 
         {/* PRODUCT 3 */}
-        <div className='item-ctn'>
+        <div className='item-ctn col-s-6 col-4'>
           <h2>iPhone X 64GB</h2>
           <img src='https://www.backmarket.de/cdn-cgi/image/format%3Dauto%2Cquality%3D75%2Cwidth%3D640/https://d1eh9yux7w8iql.cloudfront.net/product_images/36833_4776bbd6-9959-4ea1-85e1-3214d47d481f.jpg?t=resize&h=600&w=800'
           className='product-img' />
@@ -49,7 +49,7 @@ const Main:React.FC = () => {
         </div>
 
           {/* PRODUCT 4 */}
-          <div className='item-ctn'>
+          <div className='item-ctn col-s-6 col-4'>
           <h2>Macbook Pro 16" (2019)</h2>
           <img src='https://www.backmarket.de/cdn-cgi/image/format%3Dauto%2Cquality%3D75%2Cwidth%3D1920/https://d1eh9yux7w8iql.cloudfront.net/product_images/374162_f5cfdc0c-18de-48aa-b150-0359bf861b48.jpg?t=resize&h=600&w=800'
           className='product-img' />
@@ -63,7 +63,7 @@ const Main:React.FC = () => {
         </div>
 
         {/* PRODUCT 5 */}
-        <div className='item-ctn'>
+        <div className='item-ctn col-s-6 col-4'>
           <h2>iPad pro 11 (2021) 3.Gen</h2>
           <img src='https://www.backmarket.de/cdn-cgi/image/format%3Dauto%2Cquality%3D75%2Cwidth%3D640/https://d1eh9yux7w8iql.cloudfront.net/product_images/474635_0c643fb4-9e2d-4fd7-a6d4-4f756cd309f0.jpg?t=resize&h=600&w=800'
           className='product-img' />
@@ -77,7 +77,7 @@ const Main:React.FC = () => {
         </div>
 
         {/* PRODUCT 6 */}
-        <div className='item-ctn'>
+        <div className='item-ctn col-s-6 col-4'>
           <h2>AirPods (2016) mit Ladecase</h2>
           <img src='https://www.backmarket.de/cdn-cgi/image/format%3Dauto%2Cquality%3D75%2Cwidth%3D640/https://d1eh9yux7w8iql.cloudfront.net/product_images/50789_5b970288-1d95-494e-a149-69779243b83a.jpg?t=resize&h=600&w=800'
           className='product-img' />
