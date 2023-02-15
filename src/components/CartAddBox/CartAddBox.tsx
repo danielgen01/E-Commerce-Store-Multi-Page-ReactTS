ae5bff22dbbe09a520aea1ef7ef97dae5dba67e3
Daniel <gendik38@gmail.com>
2023-02-15 13:33:29 +0100

    add "cart-add-box" content

--- src/components/CartAddBox/CartAddBox.tsx
+++ src/components/CartAddBox/CartAddBox.tsx
@@ -16,7 +16,22 @@ const CartAddBox:React.FC<CartAddBoxProps> = ({ toggleCartAddContainer,isCartAdd
     <div className='cart-add-box'>
     <i className='bx bx-x'  onClick={toggleCartAddContainer} id="close-add-cart-box-icon"/>
     <div className='cart-add-box-content'>
-      
+     <div className='img-ctn col-6 col-s-6'>
+    <img src='https://files.refurbed.com/ii/iphone-12-1607327724.jpg?t=resize&h=300&w=300' />
+    </div>
+    <div className='product-infos-ctn'>
+        <h1>iPhone 12</h1>
+        <h3>699€</h3>
+        <input type="number" 
+        defaultValue={1}
+        className="input-field-product-quantity"
+        readOnly={true}
+        />
+        <div className='plus-minus-icons-ctn'>
+         <i className='bx bxs-plus-square'></i> 
+         <i className='bx bxs-minus-square' ></i>
+         </div>
+    </div>
     </div>
     </div>
 
