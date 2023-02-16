08cad68679bab08fb29fcfe9c12d425693f2a0bd
Daniel <gendik38@gmail.com>
2023-02-16 19:05:28 +0100

    make addtocart function functional

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -33,6 +33,24 @@ const Cart:React.FC<CartProps> = ({ isCartOpen, toggleCart, cartItems,setCartIte
               </div>
               </>}
 
+              {cartItems.length > 0 &&
+              <>
+              
+                {cartItems.map((item:any)=> (
+                  <div key={item} className="added-cart-item-row">
+                    <div className='item-title-and-img'
+                     style={{display:"flex",flexDirection:"column"}}>
+                    <img src={item.image} id="in-cart-product-img"/>
+                    <h4>{item.title}</h4>
+                    </div>
+                    <h4>{item.quantity}</h4>
+                    <h4>{item.price}</h4>
+                    </div>
+                ))}
+                
+              
+              </> }
+
          </div>
 
         </div>
