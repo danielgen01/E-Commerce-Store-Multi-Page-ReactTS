b9678b6e7470edbcf6e43bf6e6a3b4b5f6ef3f1b
Daniel <gendik38@gmail.com>
2023-02-21 15:55:46 +0100

    new content to homepage and resp. design chnges

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,7 +7,11 @@ const Home = () => {
   return (
     <>
     <div className='Home'>
-      <h1 style={{color:"white",textAlign:"center"}}>Check out our brandnew iPhones</h1>
+      <h1 style={{color:"white",textAlign:"center",fontSize:"2.2rem"}}>Check out our brandnew iPhones</h1>
+      <div style={{display:"flex",justifyContent:"center",alignItems:"center",marginTop:"1%"}}>
+      <a href='/shop' style={{color:"#2997ff",fontSize:"2rem",textDecoration:"none"}}>Go shopping </a>
+      <i className='bx bxs-chevron-right' style={{fontSize:"2rem",color:"#2997ff"}}></i>
+      </div>
     </div>
    
 
